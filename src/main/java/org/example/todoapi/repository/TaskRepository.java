3e28e4699216ead861691179545ac337a13750b2
th3g3ntl3man <kutchukhidze.nik@gmail.com>
2026-05-24 20:51:32 +0400

    repository methods added

--- src/main/java/org/example/todoapi/repository/TaskRepository.java
+++ src/main/java/org/example/todoapi/repository/TaskRepository.java
@@ -3,6 +3,8 @@ package org.example.todoapi.repository;
 import org.example.todoapi.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {
+import java.util.List;
 
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByUserUsername(String username);
 }
