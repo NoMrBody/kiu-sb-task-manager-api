64b976ec8c10a6de94e1d401826ac2f65299e2a9
th3g3ntl3man <kutchukhidze.nik@gmail.com>
2026-05-24 20:54:50 +0400

    add password and role fields to User entity

--- src/main/java/org/example/todoapi/entity/User.java
+++ src/main/java/org/example/todoapi/entity/User.java
@@ -16,6 +16,13 @@ public class User {
     @Column(nullable = false, unique = true)
     private String username;
 
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
     @Column(nullable = false, unique = true)
     private String email;
 }
