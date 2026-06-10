180c0ffb30f13fe55dcd256a6e0fa6ff22653984
th3g3ntl3man <kutchukhidze.nik@gmail.com>
2026-06-10 22:12:53 +0400

    Enable AppSettings properties binding in TaskManagerApplication

--- src/main/java/org/example/todoapi/TaskManagerApplication.java
+++ src/main/java/org/example/todoapi/TaskManagerApplication.java
@@ -1,9 +1,12 @@
 package org.example.todoapi;
 
+import org.example.todoapi.config.AppSettings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppSettings.class)
 public class TaskManagerApplication {
 
     public static void main(String[] args) {
